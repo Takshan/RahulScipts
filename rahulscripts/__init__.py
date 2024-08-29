fe352b4e71520e00cf991150a3e417cbdfa57f13
Takshan <rahul@drugonix.com>
2024-08-29 16:44:44 +0900

    Update __init__.py

--- rahulscripts/__init__.py
+++ rahulscripts/__init__.py
@@ -1,7 +1,5 @@
 from .__version__ import __version__
-from .download import *
-from .external import *
-from .utils import *
+
 
 print(f"Package v: {__version__}")
 print("===============================================")
