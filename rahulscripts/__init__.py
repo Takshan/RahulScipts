496f93c24a3e9acfad52b69cfac7f6f98726bdbd
Takshan <mrbrahma.rahul@gmail.com>
2024-08-29 17:24:46 +0900

    update

--- rahulscripts/__init__.py
+++ rahulscripts/__init__.py
@@ -1,12 +1,55 @@
-from .__version__ import __version__
+# from .__version__ import __version__
 
 
 from .download import *
 from .external import *
 from .utils import *
 from .metrics import *
+from typing import Any
+import importlib.metadata
+from pathlib import Path
+
+__package_version = "unknown"
 
 
+def __get_package_version() -> str:
+    """Find the version of this package."""
+    global __package_version
 
-print(f"Package v: {__version__}")
+    if __package_version != "unknown":
+        # We already set it at some point in the past,
+        # so return that previous value without any
+        # extra work.
+        return __package_version
+
+    try:
+        # Try to get the version of the current package if
+        # it is running from a distribution.
+        __package_version = importlib.metadata.version("rahulscripts")
+    except importlib.metadata.PackageNotFoundError:
+        # Fall back on getting it from a local pyproject.toml.
+        # This works in a development environment where the
+        # package has not been installed from a distribution.
+        import toml
+
+        pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
+        if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
+            __package_version = toml.load(pyproject_toml_file)["tool"]["poetry"][
+                "version"
+            ]
+            # Indicate it might be locally modified or unreleased.
+            __package_version = __package_version + "+"
+
+    return __package_version
+
+
+def __getattr__(name: str) -> Any:
+    """Get package attributes."""
+    if name in ("version", "__version__"):
+        return __get_package_version()
+    else:
+        raise AttributeError(f"No attribute {name} in module {__name__}.")
+
+
+print(f"Package ver: {rahulscripts.version}")
 print("===============================================")
